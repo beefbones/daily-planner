c8282bd4eaf3f81ec4bff69dc931283961598d5b
Nolan Ross <nolanross@Nolans-MacBook-Pro.local>
2022-11-14 22:53:03 -0500

    restructured js to make more sense

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,25 +4,6 @@ var date = dayjs().format('dddd, MMMM D YYYY, h:mm:ss A');
 $("#day").html(date);
 // Create function that will only run when website loads
 $(document).ready(function () {
-    // Add event listener for when user clicks on save button
-    $(".saveBtn").on("click", function () {
-        // Create variables that target the entry of data
-        var content = $(this).siblings(".desc").val();
-        var moment = $(this).parent().attr("id");
-        // Store them in local storage
-        localStorage.setItem(moment, content);
-    })
-   // Get items from local storage
-   $("#period8 .desc").val(localStorage.getItem("period8"));
-   $("#period9 .desc").val(localStorage.getItem("period9"));
-   $("#period10 .desc").val(localStorage.getItem("period10"));
-   $("#period11 .desc").val(localStorage.getItem("period11"));
-   $("#period12 .desc").val(localStorage.getItem("period12"));
-   $("#period1 .desc").val(localStorage.getItem("period1"));
-   $("#period2 .desc").val(localStorage.getItem("period2"));
-   $("#period3 .desc").val(localStorage.getItem("period3"));
-   $("#period4 .desc").val(localStorage.getItem("period4"));
-   $("#period5 .desc").val(localStorage.getItem("period5"));
     // Create function for changing background color
     function runTime() {
         // Assign hour of day to variable
@@ -47,8 +28,27 @@ $(document).ready(function () {
                 $(this).removeClass("past");
 
             }
+
+    // Add event listener for when user clicks on save button
+    $(".saveBtn").on("click", function () {
+        // Create variables that target the entry of data
+        var content = $(this).siblings(".desc").val();
+        var moment = $(this).parent().attr("id");
+        // Store them in local storage
+        localStorage.setItem(moment, content);
+    })
+   // Get items from local storage
+   $("#period8 .desc").val(localStorage.getItem("period8"));
+   $("#period9 .desc").val(localStorage.getItem("period9"));
+   $("#period10 .desc").val(localStorage.getItem("period10"));
+   $("#period11 .desc").val(localStorage.getItem("period11"));
+   $("#period12 .desc").val(localStorage.getItem("period12"));
+   $("#period1 .desc").val(localStorage.getItem("period1"));
+   $("#period2 .desc").val(localStorage.getItem("period2"));
+   $("#period3 .desc").val(localStorage.getItem("period3"));
+   $("#period4 .desc").val(localStorage.getItem("period4"));
+   $("#period5 .desc").val(localStorage.getItem("period5"));
         })
     }
-
-    runTime();
+runTime();
 })
